eeb06e309fe52c5c4b3ecc95a5f72076b0ce7607
Indrajith S Nair <indrajithmundackal@gmail.com>
2024-09-11 04:19:48 +0530

    Add files via upload

---
+++ extractors/rtf.py
@@ -0,0 +1,16 @@
+import re
+class RTFExtractor:
+    def __init__(self, partial_data):
+        self.data = partial_data
+        self.possible_ends = []
+        for match in re.finditer(b'}', partial_data):
+            self.possible_ends.append(match.start())
+
+        print(self.possible_ends)
+    
+    def extract_file(self, path):
+        if not self.possible_ends:
+            print("No potential RTF end marker found")
+            return
+        with open(path, "wb") as file:
+            file.write(self.data[:self.possible_ends[-1] + 1])
