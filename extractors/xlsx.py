95007b666633edae2f0fc33ff5ba493a2765c32c
TimsTittus <timstittus1@gmail.com>
2024-09-11 03:57:54 +0530

    sync with main

---
+++ extractors/xlsx.py
@@ -0,0 +1,19 @@
+import re
+
+class XLSXExtractor:
+    def __init__(self, partial_data):
+        self.data = partial_data
+        self.possible_ends = []
+
+        for match in re.finditer(b'\x50\x4B\x05\x06', partial_data):
+            self.possible_ends.append(match.start())
+
+        print(self.possible_ends)
+    
+    def extract_file(self, path):
+        if self.possible_ends:
+            with open(path, "wb") as file:
+                # Write data up to and including the End of Central Directory record
+                file.write(self.data[:self.possible_ends[-1] + 22])
+        else:
+            print("No valid ZIP end structure found")
